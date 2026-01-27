95f4f1c4021b50e2328bfd8f9d76b298ecdb9750
jackerlidzs <jacker.lidz@gmail.com>
2026-01-27 10:15:19 +0700

    chore: Remove test files

--- test_gen.py
+++
@@ -1,7 +0,0 @@
-from one.img_generator import generate_image
-from one.name_generator import NameGenerator
-
-name = NameGenerator.generate()
-img = generate_image(name['first_name'], name['last_name'], doc_type='enrollment_letter')
-open('test_final.png', 'wb').write(img)
-print(f"Generated for: {name['full_name']}")
